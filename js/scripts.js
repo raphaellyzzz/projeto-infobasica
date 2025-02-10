dcae0bfd46131c4b76ec28836b7594d4b3cd1251
Arianny Raphaelly <105996659+raphaellyzzz@users.noreply.github.com>
2025-02-10 05:14:19 -0300

    up 3
    organização dos templates e ajuste de cores.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-// Função para aplicar o zoom ao clicar nos links do menu
 function aplicarZoom(e, elemento) {
     e.preventDefault(); 
     elemento.classList.add('zoom'); 
@@ -9,7 +8,6 @@ function aplicarZoom(e, elemento) {
     }, 450);
 }
 
-// Adicionando o evento de clique nos links do menu de navegação
 const links = document.querySelectorAll('nav ul li a');
 
 links.forEach(link => {
@@ -17,35 +15,3 @@ links.forEach(link => {
         aplicarZoom(e, link); 
     });
 });
-
-// Funcionalidade do menu hambúrguer
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('open'); // Alterna a classe 'open' para abrir/fechar o menu
-});
-<script>
-        let score = 0;
-
-        function checkAnswer(questionName, correctAnswer, correctText) {
-            var resultElement = document.getElementById("result" + questionName.charAt(questionName.length - 1));
-            var selectedOption = document.querySelector('input[name="' + questionName + '"]:checked');
-            
-            if (!selectedOption) {
-                resultElement.innerHTML = "Por favor, selecione uma resposta.";
-                resultElement.style.color = "red";
-                return;
-            }
-
-            if (selectedOption.value === correctAnswer) {
-                resultElement.innerHTML = "Resposta correta! " + correctText;
-                resultElement.style.color = "green";
-                score++; // Incrementa a pontuação
-            } else {
-                resultElement.innerHTML = "Resposta incorreta. A resposta correta é: " + correctText;
-                resultElement.style.color = "red";
-            }
-
-            // Atualiza a pontuação
-            document.getElementById("score").innerHTML = "Sua pontuação: " + score + " de 10";
-        }
-    </script>
